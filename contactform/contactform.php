ac9f0a6df4d619969a76f285be1fcf9bbceb34d3
benkaddour <benkaddour.abdelkarim@gmail.com>
2018-08-21 19:58:48 +0200

    Add function of the form (php) and fix element in the home

---
+++ contactform/contactform.php
@@ -0,0 +1,18 @@
+<?php
+if($_POST){
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+ 	
+ 	$to = 'contact@pathmuslim.com';
+ 	$headers = array(
+	    'From' => $email,
+	    'Reply-To' => 'contact@pathmuslim.com',
+	    'X-Mailer' => 'PHP/' . phpversion()
+	);
+	//send email
+    mail($to, $subject." - (".$name.")", $message, $headers);
+}
+?>
